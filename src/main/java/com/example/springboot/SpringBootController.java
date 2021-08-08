60a69010d74d3f824c6ee4245c6e4c55e395a12c
eyShamir <eyShamir@gmail.com>
2021-08-08 23:11:18 +0300

    1 Commit Master

---
+++ src/main/java/com/example/springboot/SpringBootController.java
@@ -0,0 +1,29 @@
+package com.example.springboot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import java.util.Date;
+@RestController
+public class SpringBootController {
+
+    @Autowired
+
+    @RequestMapping("/")
+    public String getHelloWorld(){
+        return "Hello World from SpringBoot New";
+    }
+
+    @RequestMapping("GoodBy")
+    public String GetGoodBy(){
+        return "GoodBy from SpringBoot";
+    }
+
+    @RequestMapping("ClientDeatils")
+    public String GetClientDetails (@RequestParam(name="name", required = false, defaultValue = "Ahmed") String name){
+        return "Hello " + name;
+    }
+}
